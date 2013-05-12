dac643233c386e8bbb4417eda7fea8530de0a3ac
Rob Moore <me@robdmoore.id.au>
2013-05-12 22:39:02 +0800

    Added outputting of results in the http flooder

--- SQLAzureDemo.HttpFlooder/Program.cs
+++ SQLAzureDemo.HttpFlooder/Program.cs
@@ -4,36 +4,56 @@ using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SQLAzureDemo.HttpFlooder
 {
     class Program
     {
+        private static readonly Regex Regex = new Regex(@"There are <span class=""label label-info"">(\d+)</span> that were returned with an average creation year of <span class=""label label-info"">(\d+)</span>");
         static void Main()
         {
             const string url = "http://mscloudperthdemo.azurewebsites.net/Home/Transient?q={0}";
-            ServicePointManager.DefaultConnectionLimit = 20;
+            ServicePointManager.DefaultConnectionLimit = 100;
             var random = new Random();
             var tasks = new List<Task>();
 
-            for (var i = 0; i < 20; i++)
+            for (var i = 0; i < 100; i++)
             {
                 var searchTerm = GetRandomSearchString(random);
                 var stopwatch = Stopwatch.StartNew();
                 Console.WriteLine("Beginning search for {0}", searchTerm);
                 tasks.Add(
-                    SendHttpRequest(url, searchTerm).ContinueWith(t =>
-                        {
-                            stopwatch.Stop();
-                            Console.WriteLine("Finished search for {0} with HTTP status code of {1} in {2}s", searchTerm, t.Result.StatusCode, stopwatch.Elapsed.TotalSeconds);
-                        }
-                    )
+                    SendHttpRequest(url, searchTerm)
+                        .ContinueWith(async r => await ProcessHttpResponse(stopwatch, searchTerm, r.Result)).Unwrap()
+                        .ContinueWith(s => Console.WriteLine(s.Result))
                 );
             }
 
             Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("Finished. Press any key...");
             Console.ReadLine();
+        }
+
+        private static async Task<string> ProcessHttpResponse(Stopwatch stopwatch, string searchTerm, HttpResponseMessage response)
+        {
+            stopwatch.Stop();
+
+            var s = new StringBuilder();
+            var statusCode = response.StatusCode;
+            var content = await response.Content.ReadAsStringAsync();
+            var match = Regex.Match(content);
+            
+            s.Append(string.Format("Finished search for {0} with HTTP status code of {1} in {2}s.", searchTerm, response.StatusCode, stopwatch.Elapsed.TotalSeconds));
+
+            if (statusCode == HttpStatusCode.OK)
+                if (match.Success)
+                    s.Append(string.Format(" {0} Results with average creation year of {1}.", match.Groups[1].Value, match.Groups[2].Value));
+                else
+                    s.Append("No results returned.");
+
+            return s.ToString();
         }
 
         public static async Task<HttpResponseMessage> SendHttpRequest(string url, string searchTerm)
