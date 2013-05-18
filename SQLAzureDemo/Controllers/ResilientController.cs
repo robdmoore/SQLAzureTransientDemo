7897402c4393567ea2c0908fabc66d1c3c5f4d0d
Rob Moore <me@robdmoore.id.au>
2013-05-19 00:10:06 +0800

    Added warning logging if a sql exception makes it through the transient handling

--- SQLAzureDemo/Controllers/ResilientController.cs
+++ SQLAzureDemo/Controllers/ResilientController.cs
@@ -1,7 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Web.Mvc;
 using Autofac;
 using SQLAzureDemo.App_Start.Autofac;
 using SQLAzureDemo.Database.Repositories;
+using Serilog;
 
 namespace SQLAzureDemo.Controllers
 {
@@ -11,8 +16,21 @@ namespace SQLAzureDemo.Controllers
 
         public ResilientController(IComponentContext scope)
         {
-            // Note: you would normally resolve the IMovieRepository into the controller, but this is needed for this demo to enable use of ResolveKeyed
-            _repository = scope.ResolveKeyed<IMovieRepository>(NHibernateModule.ResilientConnection);
+            try
+            {
+                // Note: you would normally resolve the IMovieRepository into the controller, but this is needed for this demo to enable use of ResolveKeyed
+                _repository = scope.ResolveKeyed<IMovieRepository>(NHibernateModule.ResilientConnection);
+            }
+            catch (Exception e)
+            {
+                if (e is SqlException)
+                {
+                    Log.Logger.Warning(e, "SQL Exception with error nos. {0}",
+                        string.Join(",", ((SqlException)e).Errors.Cast<SqlError>().Select(error => error.Number.ToString()).ToArray()));
+                    
+                }
+                ExceptionDispatchInfo.Capture(e).Throw();
+            }
         }
 
         public ActionResult Index(string q, int page = 1)
