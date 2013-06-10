d67b4117d0cd4103e5249cecaa2d330ed5d7f3e0
Rob Moore <me@robdmoore.id.au>
2013-06-10 23:15:20 +0800

    Added ReliableDbProvider library for EF transient protection

--- SQLAzureDemo/App_Start/Autofac/EntityFrameworkModule.cs
+++ SQLAzureDemo/App_Start/Autofac/EntityFrameworkModule.cs
@@ -2,6 +2,8 @@
 using Autofac;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using ReliableDbProvider;
+using ReliableDbProvider.SqlAzureWithTimeoutRetries;
 using SQLAzureDemo.App_Start.EntityFramework;
 using Autofac.Integration.Mvc;
 using SQLAzureDemo.App_Start.NHibernate;
@@ -14,25 +16,22 @@ namespace SQLAzureDemo.App_Start.Autofac
         public const string TransientConnection = "eftransient";
         public const string ResilientConnection = "efresilient";
         public const string ReliableConnectionStringName = "ReliableDatabase";
+        public const string TransientConnectionStringName = "Database";
 
-        private readonly string _connectionString;
-
-        public EntityFrameworkModule(string connectionString, CloudStorageAccount storageAccount)
+        public EntityFrameworkModule(CloudStorageAccount storageAccount)
         {
-            _connectionString = connectionString;
-
             var tableClient = storageAccount.CreateCloudTableClient();
             var table = tableClient.GetTableReference(typeof(TransientRetry).Name);
             table.CreateIfNotExists();
-            ReliableSqlClientProvider.CommandRetry += (sender, args) => table.Execute(TableOperation.Insert(new TransientRetry(args)));
-            ReliableSqlClientProvider.ConnectionRetry += (sender, args) => table.Execute(TableOperation.Insert(new TransientRetry(args)));
+            SqlAzureProvider.CommandRetry += (sender, args) => table.Execute(TableOperation.Insert(new TransientRetry(args)));
+            SqlAzureProvider.ConnectionRetry += (sender, args) => table.Execute(TableOperation.Insert(new TransientRetry(args)));
 
             System.Data.Entity.Database.SetInitializer(new DontPerformCodeFirstMigrations());
         }
 
         protected override void Load(ContainerBuilder builder)
         {
-            builder.Register(c => new ModelContext(_connectionString))
+            builder.Register(c => new ModelContext(TransientConnectionStringName))
                 .Keyed<IModelContext>(TransientConnection)
                 .InstancePerHttpRequest();
 
