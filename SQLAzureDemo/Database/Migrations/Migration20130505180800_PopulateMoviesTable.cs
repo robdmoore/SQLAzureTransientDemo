a228de6dbf94a25e0d8f5243ba61b5853b459606
Rob Moore <me@robdmoore.id.au>
2014-03-23 22:47:10 +0800

    Fixing the migration so it works again

--- SQLAzureDemo/Database/Migrations/Migration20130505180800_PopulateMoviesTable.cs
+++ SQLAzureDemo/Database/Migrations/Migration20130505180800_PopulateMoviesTable.cs
@@ -9,17 +9,28 @@ using SQLAzureDemo.Database.Models;
 
 namespace SQLAzureDemo.Database.Migrations
 {
+    public class SearchJson
+    {
+        public MovieJson[] Search { get; set; }
+    }
+
+    public class MovieJson
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+    }
+
     public class Migration20130505180800_PopulateMoviesTable : IScript
     {
         public string ProvideScript(IDbConnection connection)
         {
             var client = new HttpClient();
             var titleSearches = new[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-            var movies = new List<Movie>();
+            var movies = new List<MovieJson>();
             foreach (var titleSearch in titleSearches)
             {
-                var result = client.GetStringAsync(string.Format("http://omdbapi.org/?limit=10&title={0}&type=json", titleSearch)).Result;
-                movies.AddRange(JsonConvert.DeserializeObject<Movie[]>(result));
+                var result = client.GetStringAsync(string.Format("http://omdbapi.com/?s={0}%20", titleSearch)).Result;
+                movies.AddRange(JsonConvert.DeserializeObject<SearchJson>(result).Search);
             }
 
             using (var command = connection.CreateCommand())
@@ -33,7 +44,7 @@ namespace SQLAzureDemo.Database.Migrations
                         s.Append(", ");
                     s.Append(string.Format("(@title{0}, @year{0})", i));
                     command.Parameters.Add(new SqlParameter(string.Format("title{0}", i), movies[i].Title));
-                    command.Parameters.Add(new SqlParameter(string.Format("year{0}", i), movies[i].Year));
+                    command.Parameters.Add(new SqlParameter(string.Format("year{0}", i), movies[i].Year.Substring(0, 4)));
                 }
 
                 command.CommandText = s.ToString();
